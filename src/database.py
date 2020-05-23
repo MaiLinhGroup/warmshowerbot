b61e884a02bb36196b79171009547c8c873b7c0b
Mai Linh Nguyen <mailinh.nguyen@bcgdv.com>
2020-05-23 21:52:46 +0200

    Add sqlite as database

---
+++ src/database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect('app_data.db')
+
+# Create cursor object with execute method to perform SQL command
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
+
+
+if __name__ == "__main__":
+    print("Create database to store app data.")
